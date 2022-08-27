7950c444429f8bc8c43e7390f020d310fda5b1a2
Truong Gia Linh <nhoquenroi2002@gmail.com>
2022-08-27 16:57:59 +0700

    create StudentRepository

---
+++ src/main/java/org/aibles/student/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package org.aibles.student.repository;
+
+import org.aibles.student.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
